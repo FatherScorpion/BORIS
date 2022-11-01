a4377ab605acea2a72554b36f2bf72eeccec5b4a
FatherScorpion <s19008@tokyo.kosen-ac.jp>
2022-11-01 11:07:09 +0900

    yeah

--- main.py
+++ main.py
@@ -4,36 +4,35 @@ import time
 nowX=0
 nowY=0
 
-def moveX(a): #X方向(横向き)にa度動かす
-  # ここを追記・修正
+def moveX(a):
   servoMotors[0].setAngle(a)
 
-def moveY(a): #Y方向(縦向き)にa度動かす
-  # ここを追記・修正
+def moveY(a):
   servoMotors[1].setAngle(a)
 
 servoMotors = []
 
 servoMotors.append(ServoMotor(Channel=0, ZeroOffset=0))
 servoMotors.append(ServoMotor(Channel=1, ZeroOffset=0))
-servoMotors.append(ServoMotor(Channel=3, ZeroOffset=0))
+servoMotors.append(ServoMotor(Channel=2, ZeroOffset=0))
 
-for i in range(5):
+for i in range(180):
   servoMotors[0].setAngle(nowX)
   servoMotors[1].setAngle(nowY)
-  servoMotors[2].setAngle(i*90)
-  nowX+=90
-  nowY+=90
-  time.sleep(1)
+  servoMotors[2].setAngle(nowX)
+  nowX=i
+  nowY=i
+  print(i)
+  time.sleep(0.01)
 servoMotors[0].setAngle(0)
 servoMotors[1].setAngle(0)
 servoMotors[2].setAngle(0)
 nowX=0
 nowY=0
 
-for i in range(4):
-  moveX(90)
-  moveY(90)
-  time.sleep(1)
+for i in range(360):
+  moveX(1)
+  moveY(1)
+  time.sleep(0.01)
 moveX(-360)
 moveY(-360)
