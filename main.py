932d32eff2438690f384a0bac1cc1012432bbdca
FatherScorpion <FatherScorpion@outlook.jp>
2022-11-16 13:07:52 +0900

    a

--- main.py
+++ main.py
@@ -17,22 +17,18 @@ servoMotors.append(ServoMotor(Channel=1, ZeroOffset=0))
 servoMotors.append(ServoMotor(Channel=2, ZeroOffset=0))
 
 for i in range(180):
-  servoMotors[0].setAngle(nowX)
-  servoMotors[1].setAngle(nowY)
-  servoMotors[2].setAngle(nowX)
-  nowX=i
-  nowY=i
+  servoMotors[0].setAngle(i)
+  servoMotors[1].setAngle(i)
+  servoMotors[2].setAngle(i)
   print(i)
   time.sleep(0.01)
 servoMotors[0].setAngle(0)
 servoMotors[1].setAngle(0)
 servoMotors[2].setAngle(0)
-nowX=0
-nowY=0
 
-for i in range(360):
+for i in range(180):
   moveX(1)
   moveY(1)
   time.sleep(0.01)
-moveX(-360)
-moveY(-360)
+moveX(-180)
+moveY(-180)
