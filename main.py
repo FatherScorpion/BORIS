c6e62bebbe0ae8d71f36ad5d66ae355e9a6b8fb9
FatherScorpion <FatherScorpion@outlook.jp>
2023-02-16 21:57:53 +0900

    hoge

--- main.py
+++ main.py
@@ -8,6 +8,13 @@ import sys
 # ここからグローバル変数
 nowX=0
 nowY=0
+minX=0
+minY=0
+maxX=180
+maxY=90
+defX=90
+defY=0
+
 mode=0 #0=停止,1=赤を打つ,2=黄色を打つ,3=人類は全員敵
 swPin=23
 ledPin=[4,17,7,22]
@@ -19,18 +26,18 @@ shotAngle=100 #引き金を引ける角度
 # ここから関数
 def moveX(a):
   nowX=nowX+a
-  if nowX>180:
-    nowX=180
-  elif nowX<0:
-    nowX=0
+  if nowX>maxX:
+    nowX=maxX
+  elif nowX<minX:
+    nowX=minX
   servoMotors[0].setAngle(nowX)
 
 def moveY(a):
   nowY=nowY+a
-  if nowY>90:
-    nowY=90
-  elif nowY<0:
-      nowY=0
+  if nowY>maxY:
+    nowY=maxY
+  elif nowY<minY:
+      nowY=minY
   servoMotors[1].setAngle(nowY)
 
 def turnOnLedByMode():
@@ -80,4 +87,23 @@ moveY(-180)
 while 1:
   checkSwitch()
   turnOnLedByMode()
+  checkGunlock()
+  if mode==0:
+    servoMotors[0].setAngle(defX)
+    servoMotors[1].setAngle(defY)
+    nowX=defX
+    nowY=defY
+  if mode==1:
+    servoMotors[0].setAngle(maxX)
+    servoMotors[1].setAngle(maxY)
+    nowX=maxX
+    nowY=maxY
+  if mode==2:
+    servoMotors[0].setAngle(minX)
+    servoMotors[1].setAngle(minY)
+    nowX=minX
+    nowY=minY
+  if mode==3:
+    shot()
+    time.sleep(5)
 # ここまでループ処理
